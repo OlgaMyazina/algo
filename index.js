002ffacbe5091a441ecc79c4df7ae0bd339d7503
Olga <olga.myazina@outlook.com>
2018-10-27 15:26:17 +0300

    сортировка выбором

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
-const arr = [2, 5, 1, 4, 9, 2, 8];
+let arr = [2, 5, 1, 4, 9, 2, 8];
+//const arr = getArr();
 console.log(arr);
 function bubbles(arr) {
   const lenght = arr.length;
@@ -15,4 +16,40 @@ function bubbles(arr) {
   return arr;
 }
 
-console.log(bubbles(arr));
+function selected(arr) {
+  //предположим, что массив из положительных чисел
+  let minValue, minIndex;
+  const newArr = [];
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    minValue = arr[0];
+    minIndex = 0;
+    for (let j = 0; j < arr.length; j++) {
+      //находим минимальный элемент
+      if (arr[j] < minValue) {
+        minValue = arr[j];
+        minIndex = j;
+      }
+    }
+    newArr[i] = minValue;
+    arr.splice(minIndex, 1);
+    minIndex = i;
+  }
+  return newArr;
+}
+console.log(selected(arr));
+
+/*
+function getArr() {
+  const arr = document.querySelector('#inputArr').split(',');
+  return arr;
+}
+function setArr(array) {
+  const outForArr = document.querySelector('#outputArr');
+  outForArr.textContent = array.join(', ');
+}
+document.querySelector('button').addEventListener('click', () => {
+  console.log('В разработке');
+});
+
+*/
